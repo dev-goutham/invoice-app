085d94c8d50b1c97d8fdaa8fb90c2074c6699487
dev-goutham <dev.gouthamram95@gmail.com>
2022-08-01 16:01:58 +0530

    added styled-components

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import GlobalStyles from './global/GlobalStyles';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <GlobalStyles theme='dark' />
     <App />
   </React.StrictMode>,
 );
