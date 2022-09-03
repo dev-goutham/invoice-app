41b6ae3c33db2cce56753fd37970bac5cfbfb982
dev-goutham <dev.gouthamram95@gmail.com>
2022-09-03 12:59:36 +0530

    possible cors solution

--- api/functions/get-invoices.ts
+++ api/functions/get-invoices.ts
@@ -3,6 +3,12 @@ import { Handler } from '@netlify/functions';
 export const handler: Handler = async (event, context) => {
   return {
     statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers':
+        'Origin, X-Requested-With, Content-Type, Accept',
+      'Access-Control-Allow-Methods': 'GET',
+    },
     body: JSON.stringify(event),
   };
 };
--- api/functions/protected.ts
+++ api/functions/protected.ts
@@ -6,7 +6,10 @@ export const handler: Handler = verifyJwt(async (event, context) => {
   return {
     status: 200,
     headers: {
-      'Access-Control-Allow-Origin': '*', // Allow from anywhere
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers':
+        'Origin, X-Requested-With, Content-Type, Accept',
+      'Access-Control-Allow-Methods': 'GET',
     },
     body: JSON.stringify(claims),
   };
