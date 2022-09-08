cd3bce364c58ff466e0899e7d8e1485487839497
dev-goutham <dev.gouthamram95@gmail.com>
2022-09-08 11:53:52 +0530

    fixed ts bug

--- src/components/StatusBadge/styles.ts
+++ src/components/StatusBadge/styles.ts
@@ -14,28 +14,16 @@ const StyledStatusBadge = styled.div<Props>`
   gap: 6px;
   font-weight: bold;
   color: ${({ status }) =>
-    status === 'due'
-      ? 'rgba(255, 143, 0)'
-      : status === 'draft'
-      ? 'rgb(223, 227, 250)'
-      : 'rgba(51, 214, 159)'};
+    status === 'due' ? 'rgba(255, 143, 0)' : 'rgba(51, 214, 159)'};
   background-color: ${({ status }) =>
-    status === 'due'
-      ? 'rgba(255, 143, 0, 0.06)'
-      : status === 'draft'
-      ? 'rgba(223, 227, 250, 0.06)'
-      : 'rgba(51, 214, 159, 0.06)'};
+    status === 'due' ? 'rgba(255, 143, 0, 0.06)' : 'rgba(51, 214, 159, 0.06)'};
   .dot {
     display: inline-block;
     height: 6px;
     width: 6px;
     border-radius: 50%;
     background-color: ${({ status }) =>
-      status === 'due'
-        ? 'rgba(255, 143, 0)'
-        : status === 'draft'
-        ? 'rgb(223, 227, 250)'
-        : 'rgba(51, 214, 159)'};
+      status === 'due' ? 'rgba(255, 143, 0)' : 'rgba(51, 214, 159)'};
   }
 `;
 
